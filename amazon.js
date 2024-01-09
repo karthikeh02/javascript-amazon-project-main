c93b631544c223dfb3023620bf50aea4391ec9e8
karthik <karthiraj453@gmailcom>
2024-01-09 16:16:12 +0530

    productid with consolelog created

---
+++ amazon.js
@@ -0,0 +1,88 @@
+ 
+
+let productsHTML1 = '';
+
+products.forEach((product) => {
+    productsHTML1 += `
+     <div class="product-container">
+        <div class="product-image-container">
+        <img class="product-image"
+            src="${product.image}">
+        </div>
+
+        <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+        <img class="product-rating-stars"
+            src="images/ratings/rating-${product.rating.stars *10}.png">
+        <div class="product-rating-count link-primary">
+            ${product.rating.count}
+        </div>
+        </div>
+
+        <div class="product-price">
+        $${product.priceCents /100}
+        </div>
+
+        <div class="product-quantity-container">
+        <select>
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+        </select>
+        </div>
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+        </div>
+
+        <button class="add-to-cart-button 
+        button-primary 
+         js-add-to-cart"
+         data-product-id="${product.id}">
+        Add to Cart
+        </button>
+    </div>
+    `;
+})
+
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML1;
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+    
+    button.addEventListener('click',() => {
+
+        const productsid = button.dataset.productId;
+
+        let matchingId;
+
+        cart1.forEach((item) => {
+            if(productsid === item.productId){
+               matchingId = item;
+            }
+        })
+        if (matchingId){
+            matchingId.quantity += 1;
+        } else {
+            cart1.push({
+                productId : productsid,
+                quantity : 1,
+            });
+        }
+        console.log(cart1);
+    });
+});
